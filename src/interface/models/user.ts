c49902066c61c04a9979c3062150354b463a2a8b
William <william1006wei@Gmail.com>
2023-02-18 16:06:51 +0800

    Add UserUtil class and refactor User interface to support both camelCase and underscore_case

--- src/interface/models/user.ts
+++ src/interface/models/user.ts
@@ -5,18 +5,50 @@
  */
 
 export interface Tokens {
-	kind: string;
-	accessToken: string;
-	tokenSecret?: string;
+  kind: string;
+  accessToken: string;
+  tokenSecret?: string;
 }
 
-export interface IUser {
-	id: number;
-	name: string;
-	email: string;
-	password_hash: string;
-	created_at: Date;
-  
+type CammelCaseUser = {
+  id: number;
+  name: string;
+  email: string;
+  passwordHash: string;
+  createdAt: Date;
+};
+
+type UnderscoreCaseUser = {
+  id: number;
+  name: string;
+  email: string;
+  password_hash: string;
+  created_at: Date;
+};
+
+// User is the interface for User model
+type User = CammelCaseUser & UnderscoreCaseUser;
+
+export class UserUtil {
+  public static toCammelCase(user: CammelCaseUser & UnderscoreCaseUser): CammelCaseUser {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      passwordHash: user.password_hash || user.passwordHash,
+      createdAt: user.created_at || user.createdAt,
+    };
+  }
+
+  public static toUnderscoreCase(user: CammelCaseUser & UnderscoreCaseUser): UnderscoreCaseUser {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      password_hash: user.password_hash || user.passwordHash,
+      created_at: user.created_at || user.createdAt,
+    };
+  }
 }
 
-export default IUser;
+export default User;
